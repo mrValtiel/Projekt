e6f3ad0bd229c649378e7ea9bf685328ea71e5f1
= <=>
2017-11-01 10:41:17 +0100

    Database connection

---
+++ src/main/java/test/bookingdemo/DemoController.java
@@ -0,0 +1,14 @@
+package test.bookingdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController
+{
+    @RequestMapping("/hello")
+    public String hello()
+    {
+        return "hello";
+    }
+}
---
+++ src/main/java/test/bookingdemo/HotelBooking.java
@@ -0,0 +1,54 @@
+package test.bookingdemo;
+
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class HotelBooking
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    private String hotelName;
+    private double pricePerNight;
+    private int nbOfNights;
+
+    public HotelBooking(){}
+
+    public HotelBooking(String hotelName, double pricePerNight, int nbOfNights)
+    {
+        this.hotelName = hotelName;
+        this.pricePerNight = pricePerNight;
+        this.nbOfNights = nbOfNights;
+    }
+
+    public int getNbOfNights()
+    {
+        return nbOfNights;
+    }
+
+    public double getPricePerNight()
+    {
+        return pricePerNight;
+    }
+
+    public String getHotelName()
+    {
+        return hotelName;
+    }
+
+    public double getTotalPrice()
+    {
+        return pricePerNight * nbOfNights;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+}
